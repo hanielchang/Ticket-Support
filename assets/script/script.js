1e6c728a931df95ebb5f768452263bed8088dce0
rrayachhetri <rrayachhetri@gmail.com>
2021-09-21 22:48:57 -0700

    modal

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,9 +1,15 @@
-var usernameEL = docuyment.querySelector("#username");
-var passwordEl = document.querySelector("#password");
+var usernameEL = document.querySelector("#username");
+var userformEl = document.querySelector("#user-form");
 
-// var submitHandler = (event) => {
-//     event.preventDefault(event)
-//   var usernameInput = usernameEL.value;
+var submitHandler = (event) => {
+    event.preventDefault(event)
+  var usernameInput = usernameEL.value;
 //   var passwordInput = passwordEl.value;
-// //   if()
-// }
+  if(usernameInput) {
+      console.log(usernameInput);
+      usernameEL.textContent = '';
+  } else {
+      alert("Please Enter the username");
+  }
+}
+userformEl.addEventListener("submit", submitHandler);
