292e7138ca5b98bca4a4220700744ae9cf2aacfa
rrayachhetri <rrayachhetri@gmail.com>
2021-09-21 19:43:33 -0700

    initial files

---
+++ assets/script/script.js
@@ -0,0 +1,9 @@
+var usernameEL = docuyment.querySelector("#username");
+var passwordEl = document.querySelector("#password");
+
+// var submitHandler = (event) => {
+//     event.preventDefault(event)
+//   var usernameInput = usernameEL.value;
+//   var passwordInput = passwordEl.value;
+// //   if()
+// }
