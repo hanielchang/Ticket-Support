8d6abf2e8f35f755d8ab1103743bd779f90696d9
rrayachhetri <rrayachhetri@gmail.com>
2021-09-24 17:48:26 -0700

    sign up template

--- assets/script/script.js
+++ assets/script/script.js
@@ -2,10 +2,13 @@ var usernameEL = document.querySelector("#username");
 var passwordEL = document.querySelector("#password");
 var userformEl = document.querySelector("#user-form");
 var modal = document.querySelector(".modal-fade");
-var signUpBtn = document.querySelector("#modal");
+var ticketContainerEl = document.querySelector("#ticket-container");
+// buttons
+var createBtn = document.querySelector("#create-account");
 var closeBtn = document.querySelector(".close");
-var modalClose = document.querySelector("#closeBtn")
-
+var modalClose = document.querySelector("#modal-close");
+var loginBtn = document.querySelector("#login-btn");
+var signUpBtn = document.querySelector("sign-up");
 
 //fetch api for login
 var getTicket = (user) => {
@@ -15,7 +18,7 @@ var getTicket = (user) => {
     if (response.ok) {
       response.json().then(function (data) {
         console.log(data);
-
+        displayLogin(data, tickets);
         // Another apiUrl for ticket  details
         apiUrl = "";
         fetch(apiUrl).then(function (response2) {
@@ -34,15 +37,16 @@ var getTicket = (user) => {
   })
 };
 
-
+// check-in username password
 var submitHandler = (event) => {
-  event.preventDefault(event)
+  event.preventDefault();
   var username = usernameEL.value;
   var password = passwordEL.value;
   //   var passwordInput = passwordEl.value;
   if (username && password) {
     console.log(username);
     console.log(password);
+    getTicket(username);
 
     usernameEL.textContent = '';
     passwordEL.textContent = '';
@@ -53,7 +57,30 @@ var submitHandler = (event) => {
 userformEl.addEventListener("submit", submitHandler);
 
 
-signUpBtn.addEventListener('click', () => {
+//create a new Account
+var modalSubmitHandler = (event) => {
+  event.preventDefault();
+  var nameFirst = document.querySelector("#firstname").value;
+  var nameLast = document.querySelector("#lastname").value;
+  var email = document.querySelector("#email").value;
+  var passNew = document.querySelector("#newpassword").value;
+
+}
+
+
+
+var displayLogin = (tickets) => {
+
+for( let i = 0; i < tickets.length; i++) {
+  var ticketEl = document.createElement("a");
+  ticketEl.setAttribute=("target","_blank" );
+}
+  // clear 
+}
+
+
+loginBtn.addEventListener('submit', displayLogin);
+createBtn.addEventListener('click', () => {
   modal.style.display = "grid";
 });
 closeBtn.addEventListener('click', () => {
